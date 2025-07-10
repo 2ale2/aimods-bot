cd7ee74f0f97a48faa59b5b484ff0c78f57fb462
2ale2 <76873631+2ale2@users.noreply.github.com>
2025-07-10 19:46:06 +0200

    refactored automatic_tasks.py and constants.py. now the hardest part

---
+++ aimods_bot/src/helpers/filters.py
@@ -0,0 +1,18 @@
+import telegram
+from telegram.ext.filters import MessageFilter, ChatType
+from aimods_bot.src.helpers.utils.file_utils import get_data_from_json
+
+
+class ChannelMessageForRecapFilter(MessageFilter):
+    def filter(self, message: telegram.Message):
+        if message.chat.type is not ChatType.CHANNEL:
+            return False
+        if not message.text and not message.caption:
+            return False
+        hashtags = get_data_from_json("hashtags")["platforms"]
+        l = []
+        for el in hashtags:
+            l.extend(hashtags[el])
+        if any(hashtag in message.text for hashtag in l):
+            return True
+        return False
