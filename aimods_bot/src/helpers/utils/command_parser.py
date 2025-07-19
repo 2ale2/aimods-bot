b6583ab0d75ed560acd20cd7300d85b6c5283a85
Alessandro <2ale2>
2025-07-19 20:51:41 +0200

    fixed reply_to_message user managing for commands

--- aimods_bot/src/helpers/utils/command_parser.py
+++ aimods_bot/src/helpers/utils/command_parser.py
@@ -3,7 +3,7 @@ from telegram import Update
 from telegram.ext import ContextTypes
 
 from aimods_bot.src.helpers.utils.time_utils import parse_duration
-from aimods_bot.src.helpers.utils.telegram_utils import resolve_chat_member, is_username, normalize_user
+from aimods_bot.src.helpers.utils.telegram_utils import resolve_chat_member, is_username, normalize_user, is_user_id
 from aimods_bot.src.helpers.loggers import logger
 from aimods_bot.src.core.exceptions import MissingConfigurationException
 
@@ -40,6 +40,9 @@ async def parse_command(
         log.warning(f"⚠️ Comando non valido: {full_command}")
         return None
 
+    replied = update.effective_message.reply_to_message
+    replied = replied if replied and not replied.forum_topic_created else None
+
     extracted = {"action": match.group(1), "username_not_participant": False}
     gidx = 2
 
@@ -53,6 +56,10 @@ async def parse_command(
         else:
             extracted[param] = match.group(gidx) or ""
             gidx += 1
+
+    if not extracted["user"]:
+        if replied:
+            extracted["user"] = replied.from_user.username or replied.from_user.id
 
     if "duration" in extracted:
         extracted["duration"] = await parse_duration(extracted["duration"])
@@ -71,24 +78,17 @@ async def parse_command(
             "source": "unknown"
         }
     else:
-        replied = update.effective_message.reply_to_message
-        replied = replied if replied and not replied.forum_topic_created else None
-
-        if replied:
-            extracted["member"] = normalize_user(replied.from_user)
-        else:
-            # se match, allora extracted["user"]
-            # noinspection PyUnboundLocalVariable
-            extracted["member"] = {
-                "id": extracted["user"] if (i := not is_username(extracted["user"])) else None,
-                "username": extracted["user"] if not i else None,
-                "first_name": "",
-                "chat_member_instance": None,
-                "user_instance": None,
-                "source": "unknown"
-            }
-            if not i:
-                extracted["username_not_participant"] = True
+        i = is_user_id(extracted["user"]) or None
+        extracted["member"] = {
+            "id": extracted["user"] if i is True else None,
+            "username": extracted["user"] if i is False else None,
+            "first_name": "",
+            "chat_member_instance": None,
+            "user_instance": None,
+            "source": "unknown"
+        }
+        if i is False:
+            extracted["username_not_participant"] = True
 
     return {
         "action": extracted.get("action"),
