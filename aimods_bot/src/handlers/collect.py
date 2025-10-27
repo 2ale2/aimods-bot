c71d91d400b298f401d551d1f5a62f33116ab1c7
Alessandro <2ale2>
2025-10-27 13:12:54 +0100

    adding troubleshooting handler

--- aimods_bot/src/handlers/collect.py
+++ aimods_bot/src/handlers/collect.py
@@ -5,6 +5,7 @@ from aimods_bot.src.core.customcontext import CustomContext
 from aimods_bot.src.handlers.commands.admin.moderation_handler import moderation_handler
 from aimods_bot.src.handlers.commands.admin.service_handler import multi_media_echo_handler, service_handler, \
     test_command_handler
+from aimods_bot.src.handlers.commands.admin.troubleshooting_handlers import reset_user_data
 from aimods_bot.src.handlers.conversations.private_conversation_handlers import alert_handler, private_conversation_handler, \
     close_button_handler, TestHandler
 from aimods_bot.src.handlers.conversations.join_handler import new_member_handler
@@ -22,7 +23,8 @@ active_handlers = [
     channel_posts_capture_handler,
     new_member_handler,
     private_conversation_handler,
-    close_button_handler
+    close_button_handler,
+    reset_user_data
 ]
 
 all_handlers = [
